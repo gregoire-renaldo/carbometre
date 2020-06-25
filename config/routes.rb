528504c242c2b7f6a82183640c8e8ed7ca7a7c48
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-06-25 19:12:55 +0200

    controller footprints updated with values for calculations

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :footprints
   devise_for :users
   root to: 'pages#home'
+  get "/pages/professional_bilan" => "pages#professional_bilan"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
