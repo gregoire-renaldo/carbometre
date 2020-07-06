73e92bccd463d0a9d553ec5788bbd1054788b61c
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-07-06 16:27:58 +0200

    chrome

--- app/controllers/footprints_controller.rb
+++ app/controllers/footprints_controller.rb
@@ -51,7 +51,7 @@ class FootprintsController < ApplicationController
   private
 
   def footprint_params
-    params.require(:footprint).permit(:ble, :carotte, :boeuf)
+    params.require(:footprint).permit(:ble, :carotte, :boeuf, :riz, :saladesaison, :saladess, :tomatesfr, :tomatesfrss, :veau, :laitvache, :poulet, :oeufs, :electricite, :gazkwh, :gazm, :fioul, :granulebois, :buchebois, :eau, :aspirateur, :congelateur, :refrigerateur250l, :fourelectrique, :lavelinge, :microonde, :smartphone, :ordinateur, :tgv, :terelectrique, :terthermique, :intercite, :metro, :bus, :avion, :avionregional)
   end
 
   def calculations
