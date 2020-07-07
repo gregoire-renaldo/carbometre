948f04f38beee1b03e8744b22da1c7ad993f6f8a
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-07-07 16:19:59 +0200

    subtotal ok

--- app/javascript/packs/calculations.js
+++ app/javascript/packs/calculations.js
@@ -4,49 +4,72 @@
 
 
 // alimentaire
+// fonction calcul + apeller fonction pour chaque field
+
+const totalAlimBox = document.getElementById('totalAlimBox')
+function totAlim() {
+  const numberble = Number(scoreBle.textContent);
+  const numbercarotte = Number(scoreCarotte.textContent);
+  const numberriz = Number(scoreRiz.textContent);
+  const numberPomedeterre = Number(scorePommedeterre.textContent);
+  // riz patates saladess tomatesfr tomatesfrss Veau Laitvache Poulet Oeufs
+  const sum = numberble + numbercarotte + numberriz + numberPomedeterre
+  totalAlimBox.innerText = sum.toFixed(2)
+}
+
 // field ble
-let userBle = document.getElementById('userBle');
+const userBle = document.getElementById('userBle');
 const blecc = 0.439
-const resultBle = document.getElementById('scoreble');
+const scoreBle = document.getElementById('scoreble');
 userBle.addEventListener("input", multiplyBle);
 function multiplyBle() {
   let one = parseFloat(userBle.value) || 0;
-  let ccble = one * blecc;
-  resultBle.innerHTML = ccble;
+  let ccble = parseFloat(one * blecc).toFixed(2);
+  scoreBle.innerHTML = ccble;
+  totAlim()
 }
+
 
 // field carotte
 let userCarotte = document.getElementById('userCarotte');
 const carottecc = 0.0706
-const resultCarotte = document.getElementById('scoreCarotte');
+const scoreCarotte = document.getElementById('scoreCarotte');
 userCarotte.addEventListener("input", multiplyCarotte);
 function multiplyCarotte() {
   let one = parseFloat(userCarotte.value) || 0;
-  let cccarotte = one * carottecc;
-  resultCarotte.innerHTML = cccarotte;
+  let cccarotte = parseFloat(one * carottecc).toFixed(2);
+  scoreCarotte.innerHTML = cccarotte;
+  totAlim()
 }
 
 // field riz
 let userRiz = document.getElementById('userRiz');
 const rizcc = 3.59
-const resultRiz = document.getElementById('scoreRiz');
+const scoreRiz = document.getElementById('scoreRiz');
 userRiz.addEventListener("input", multiplyRiz);
 function multiplyRiz() {
   let one = parseFloat(userRiz.value) || 0;
-  let ccriz = one * rizcc;
-  resultRiz.innerHTML = ccriz;
+  let ccriz = parseFloat(one * rizcc).toFixed(2);
+  scoreRiz.innerHTML = ccriz;
+  totAlim()
 }
 
 // field pomme de terre fr
 let userPommeDeTerre = document.getElementById('userPommeDeTerre');
 const pommedeterrecc = 0.0861
-const resultPommedeterre = document.getElementById('scorePommeDeTerre');
+const scorePommedeterre = document.getElementById('scorePommeDeTerre');
 userPommeDeTerre.addEventListener("input", multiplyPatate);
 function multiplyPatate() {
   let one = parseFloat(userPommeDeTerre.value) || 0;
-  let pommedeterre = one * pommedeterrecc;
-  resultPommedeterre.innerHTML = pommedeterre;
+  let pommedeterre = parseFloat(one * pommedeterrecc).toFixed(2);
+  scorePommedeterre.innerHTML = pommedeterre;
+  totAlim()
 }
+
+
+
+
+
 
 // energie
 // field electricité
@@ -56,8 +79,9 @@ let resultElectricite = document.getElementById('scoreelec');
 userElectricite.addEventListener("input", multiplyElec);
 function multiplyElec() {
   let one = parseFloat(userElectricite.value) || 0;
-  let cc = one * elecricitecc;
+  let cc = parseFloat(one * elecricitecc).toFixed(2);
   resultElectricite.innerHTML =  cc;
+  totAlim()
 };
 // Autre valeur
 // Autres moyens de déplacement
