3b40efbbd244183ea5e086424ce0d8d438d0b12e
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-07-15 01:06:50 +0200

    fix1 model

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -51,31 +51,31 @@ class Footprint < ApplicationRecord
   end
 
   def total_score
-    (self.ble*0.439) + (self.boeuf*15) + (self.carotte*0.0706) + (self.riz*3.59) + (self.patate*0.0861) + (self.saladesaison*0.326) + (self.saladess*11.05) + (self.tomatesfr*0.177) + (self.tomatesfrss*2.23) + (self.veau*6.5) + (self.laitvache*0.9) + (self.poulet*2.14) + (self.oeufs*1.72) + (self.electricite*0.571) + (self.gazkwh*0.205) + (self.gazm*2.2) + (self.fioul*3.25) + (self.granulebois*0.11) + (self.buchebois*0.114) + (self.eau*0.132) + (self.aspirateur*52) + (self.congelateur*415) + (self.refrigerateur250l*300) + (self.fourelectrique*217) + (self.lavelinge*305) + (self.microonde*100) + (self.smartphone*15) + (self.ordinateur*150) + (self.tgv*0.00369) + (self.terelectrique*0.00891) + (self.terthermique*0.0798) + (self.intercite*0.0056) + (self.metro*0.0057) + (self.bus*0.092166667) + (self.avion*0.258) + (self.avionregional*0.453) + (self.essence*2.8) + (self.gazole*3.16) + (self.gpl*1.86) + (self.e85*1.68) + (self.electrique*0.071) + self.autre + self.valeur2
+    ((self.ble*0.439) + (self.boeuf*15) + (self.carotte*0.0706) + (self.riz*3.59) + (self.patate*0.0861) + (self.saladesaison*0.326) + (self.saladess*11.05) + (self.tomatesfr*0.177) + (self.tomatesfrss*2.23) + (self.veau*6.5) + (self.laitvache*0.9) + (self.poulet*2.14) + (self.oeufs*1.72) + (self.electricite*0.571) + (self.gazkwh*0.205) + (self.gazm*2.2) + (self.fioul*3.25) + (self.granulebois*0.11) + (self.buchebois*0.114) + (self.eau*0.132) + (self.aspirateur*52) + (self.congelateur*415) + (self.refrigerateur250l*300) + (self.fourelectrique*217) + (self.lavelinge*305) + (self.microonde*100) + (self.smartphone*15) + (self.ordinateur*150) + (self.tgv*0.00369) + (self.terelectrique*0.00891) + (self.terthermique*0.0798) + (self.intercite*0.0056) + (self.metro*0.0057) + (self.bus*0.092166667) + (self.avion*0.258) + (self.avionregional*0.453) + (self.essence*2.8) + (self.gazole*3.16) + (self.gpl*1.86) + (self.e85*1.68) + (self.electrique*0.071) + (self.autre) + (self.valeur2)).ceil(1)
   end
 
   def total_voiture
-    (self.essence*2.8) + (self.gazole*3.16) + (self.gpl*1.86) + (self.e85*1.68) + (self.electrique*0.071)
+    ((self.essence*2.8) + (self.gazole*3.16) + (self.gpl*1.86) + (self.e85*1.68) + (self.electrique*0.071)).ceil(2)
   end
 
   def total_autre_transport
-    (self.tgv*0.00369) + (self.terelectrique*0.00891) + (self.terthermique*0.0798) + (self.intercite*0.0056) + (self.metro*0.0057) + (self.bus*0.092166667) + (self.avion*0.258) + (self.avionregional*0.453)
+    ((self.tgv*0.00369) + (self.terelectrique*0.00891) + (self.terthermique*0.0798) + (self.intercite*0.0056) + (self.metro*0.0057) + (self.bus*0.092166667) + (self.avion*0.258) + (self.avionregional*0.453)).ceil(2)
   end
 
   def total_logement
-    (self.electricite*0.0571) + (self.gazkwh*0.205) + (self.gazm*2.2) + (self.fioul*3.25) + (self.granulebois*0.11) + (self.buchebois*0.114) + (self.eau*0.132)
+    ((self.electricite*0.0571) + (self.gazkwh*0.205) + (self.gazm*2.2) + (self.fioul*3.25) + (self.granulebois*0.11) + (self.buchebois*0.114) + (self.eau*0.132)).ceil(2)
   end
 
   def total_alimentation
-    (self.ble*0.439) + (self.boeuf*15) + (self.carotte*0.0706) + (self.riz*3.59) + (self.patate*0.0861) + (self.saladesaison*0.326) + (self.saladess*0.1105) + (self.tomatesfr*0.177) + (self.tomatesfrss*2.23) + (self.veau*6.5) + (self.laitvache*0.9) + (self.poulet*2.14) + (self.oeufs*1.72)
+    ((self.ble*0.439) + (self.boeuf*15) + (self.carotte*0.0706) + (self.riz*3.59) + (self.patate*0.0861) + (self.saladesaison*0.326) + (self.saladess*0.1105) + (self.tomatesfr*0.177) + (self.tomatesfrss*2.23) + (self.veau*6.5) + (self.laitvache*0.9) + (self.poulet*2.14) + (self.oeufs*1.72).ceil(2))
   end
 
   def total_autres
-    (self.aspirateur*52) + (self.congelateur*415) + (self.refrigerateur250l*300) + (self.fourelectrique*217) + (self.lavelinge*305) + (self.microonde*100) + (self.smartphone*15) + (self.ordinateur*150)
+    ((self.aspirateur*52) + (self.congelateur*415) + (self.refrigerateur250l*300) + (self.fourelectrique*217) + (self.lavelinge*305) + (self.microonde*100) + (self.smartphone*15) + (self.ordinateur*150)).ceil(2)
   end
 
   def total_valeur
-    self.autre  + self.valeur2
+    (self.autre  + self.valeur2).ceil(2)
   end
 
   # def calculations
