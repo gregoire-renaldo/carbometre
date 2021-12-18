c3d4ddd5355c2862fae4b7401dd795e050b9c9ae
gregoire renaldo <gregoire.renaldo@gmail.com>
2021-12-18 17:31:13 +0100

    field email

---
+++ app/javascript/packs/connect.js
@@ -0,0 +1,12 @@
+const fieldUsername = document.getElementById('usernameField')
+const fieldEmail = document.getElementById('fieldEmail')
+
+
+console.log(fieldEmail)
+
+fieldUsername.addEventListener('input', addEmail )
+function addEmail() {
+  let email = fieldUsername.value
+  fieldEmail.innerHTML = email + '@mail.com'
+  console.log(email)
+}
